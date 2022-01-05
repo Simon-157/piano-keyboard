fae292b3c4ab3e3eb9c88cbe48d13652d0ec1ea2
Simon-157 <boatengsimonjnr157@gmail.com>
2022-01-05 06:35:21 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+const note_1 = new Audio("music_notes/A.wav")
+const note_2 = new Audio("music_notes/B.wav")
+const note_3 = new Audio("music_notes/C.wav")
+const note_4 = new Audio("music_notes/D.wav")
+const note_5 = new Audio("music_notes/E.wav")
+const note_6 = new Audio("music_notes/F.wav")
+const note_7 = new Audio("music_notes/G.wav")
+const note_8 = new Audio("music_notes/A.wav")
+const note_9 = new Audio("music_notes/A.wav")
+const note_10 = new Audio("music_notes/A.wav")
+const note_11 = new Audio("music_notes/A.wav")
+
+
+const key1_play = ()=>{
+    note_1.play()
+}
+const key2_play = ()=>{
+    note_2.play()
+}
+const key3_play = ()=>{
+    note_3.play()
+}
+const key4_play = ()=>{
+    note_4.play()
+}
+const key5_play = ()=>{
+    note_5.play()
+}
+const key6_play = ()=>{
+    note_6.play()
+}
+const key7_play = ()=>{
+    note_7.play()
+}
+const key8_play = ()=>{
+    note_8.play()
+}
+
+
+
+
